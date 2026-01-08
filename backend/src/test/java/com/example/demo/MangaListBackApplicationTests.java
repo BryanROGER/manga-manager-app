a763a230c3b5d0cf3272504282c9fe51076bdbd8
BryanROGER <bryan.bruzois@gmail.com>
2026-01-08 14:30:44 +0100

    Mise en place pipeline CI avec intégration sonar cloud

--- backend/src/test/java/com/example/demo/MangaListBackApplicationTests.java
+++ backend/src/test/java/com/example/demo/MangaListBackApplicationTests.java
@@ -2,12 +2,17 @@ package com.example.demo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
+@ActiveProfiles("test") // <--- Dit à Spring d'utiliser application-test.yml
 class MangaListBackApplicationTests {
 
 	@Test
-	void contextLoads() {
+	void testPourInitialiserASupprimerPlusTard() {
+		assertTrue(true);
 	}
 
 }
